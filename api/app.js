96d4bfa70bb6275fe053075c199f9b3a9565c642
Matt Rinker <matt@canvass.com>
2020-01-30 15:19:36 -0700

    Add comments

--- api/app.js
+++ api/app.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express'); // import express (rest api package)
+const app = express(); // initialize the express app
+const port = 3000; // set the http port number
 
+// create a test route/endpoint with the GET method
+// http://localhost:3000/hello
 app.get('/hello', (req, res) => res.send('Hello World!'));
 
+// start the app listening on the specified port
 app.listen(port, () => console.log(`Pokedex API listening on port ${port}!`));
